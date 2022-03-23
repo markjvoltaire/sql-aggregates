3be6359941cfc181108d4bd218e87190d0308822
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-23 19:29:12 -0400

    test 4 and 5 passed

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,18 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT
+name,
+AVG (length) AS avg
+FROM 
+category
+RIGHT JOIN
+film_category
+ON
+film_category.category_id = category.category_id
+LEFT JOIN
+film
+ON
+film_category.film_id = film.film_id
+GROUP BY 
+name
--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,1 +1,13 @@
 -- the total run time of all inventory items if they were all played back to back
+
+
+SELECT
+SUM (length)
+FROM
+film
+INNER JOIN
+inventory
+ON
+film.film_id = inventory.film_id
+
+
