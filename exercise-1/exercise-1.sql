30ee92381b590dee36cb387bc6a1d16354a05a18
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-23 15:42:56 -0400

    test one passed

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,23 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+SELECT
+title,
+COUNT (*)
+FROM
+ film
+LEFT JOIN
+inventory
+ON
+inventory.film_id = film.film_id
+LEFT JOIN
+rental
+ON 
+rental.inventory_id = inventory.inventory_id
+GROUP BY
+film.film_id
+ORDER BY
+count DESC
+LIMIT
+10;
