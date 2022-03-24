5cf066c349e960f3832f50e65001c0785dca69cb
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-23 20:26:57 -0400

    test 8 passed

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,26 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+SELECT
+name,
+COUNT(rental_id),
+RANK() OVER (ORDER BY COUNT(rental_id) DESC)
+FROM
+category
+INNER JOIN 
+film_category
+ON
+film_category.category_id = category.category_id
+INNER JOIN
+film 
+ON
+film.film_id = film_category.film_id
+INNER JOIN 
+inventory
+ON
+film.film_id = inventory.film_id
+INNER JOIN
+rental
+ON
+inventory.inventory_id = rental.inventory_id
+GROUP BY 
+name
